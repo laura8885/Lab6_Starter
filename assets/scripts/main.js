8322cdcef1a38dbd7386f8bef53aecdcd38e7f99
laura8885 <laurakarry82@gmail.com>
2026-05-12 19:39:27 -0700

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,40 +1,21 @@
 // main.js
 
-// Run the init() function when the page has loaded
 window.addEventListener("DOMContentLoaded", init);
 
-// Starts the program, all function calls trace back here
 function init() {
-	// Get the recipes from localStorage
 	let recipes = getRecipesFromStorage();
-
-	// Add each recipe to the <main> element
 	addRecipesToDocument(recipes);
-
-	// Add the event listeners to the form elements
 	initFormHandler();
 }
 
-/**
- * Reads 'recipes' from localStorage and returns an array of
- * all of the recipes found (parsed, not in string form). If
- * nothing is found in localStorage for 'recipes', an empty array
- * is returned.
- * @returns {Array<Object>} An array of recipes found in localStorage
- */
 function getRecipesFromStorage() {
 	return JSON.parse(localStorage.getItem("recipes")) || [];
 }
 
-/**
- * Takes in an array of recipes and for each recipe creates a
- * new <recipe-card> element, adds the recipe data to that card
- * using element.data = {...}, and then appends that new recipe
- * to <main>
- * @param {Array<Object>} recipes An array of recipes
- */
 function addRecipesToDocument(recipes) {
 	const main = document.querySelector("main");
+
+	main.replaceChildren();
 
 	recipes.forEach((recipe) => {
 		const card = document.createElement("recipe-card");
@@ -43,19 +24,13 @@ function addRecipesToDocument(recipes) {
 	});
 }
 
-/**
- * Saves recipes array into localStorage
- * @param {Array<Object>} recipes An array of recipes
- */
 function saveRecipesToStorage(recipes) {
 	localStorage.setItem("recipes", JSON.stringify(recipes));
 }
 
-/**
- * Adds the necessary event handlers to <form> and the clear storage button.
- */
 function initFormHandler() {
 	const form = document.querySelector("#new-recipe");
+	const clearButton = document.querySelector("button.danger");
 
 	form.addEventListener("submit", (event) => {
 		event.preventDefault();
@@ -79,10 +54,10 @@ function initFormHandler() {
 		form.reset();
 	});
 
-	const clearButton = document.querySelector("button.danger");
+	clearButton.addEventListener("click", (event) => {
+		event.preventDefault();
 
-	clearButton.addEventListener("click", () => {
-		localStorage.clear();
-		document.querySelector("main").innerHTML = "";
+		localStorage.removeItem("recipes");
+		document.querySelector("main").replaceChildren();
 	});
 }
